521dae1d576d57847442c0ff87de4f42b9ef1834
Clarissa Audrey <clarissa.audrey@usefriz.com>
2022-09-22 18:52:21 +0700

    test(dashboard): Update test case

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,12 @@
-import { shallowMount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
 import WelcomeFriz from "@/components/WelcomeFriz.vue";
 
 describe("WelcomeFriz.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(WelcomeFriz, {
+    const wrapper = mount(WelcomeFriz, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
