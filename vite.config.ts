789de77f28cef1f9470a5006bcdebd5e8edb5a92
Clarissa Audrey <clarissa.audrey@usefriz.com>
2022-09-26 12:00:32 +0700

    feat(dashboard): Configure style sheets in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+const pathSrc = path.resolve(__dirname, "./src");
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "${pathSrc}/scss/main";` },
     },
   },
 });
