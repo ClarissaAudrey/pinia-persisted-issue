0681eb8b980e5651cd3c026122eda82f324f8d88
Clarissa Audrey <clarissa.audrey@usefriz.com>
2022-09-27 17:39:39 +0700

    feat(dashboard): Error

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,4 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
-  rules: {
-    "vue/multi-word-component-names": [
-      "error",
-      {
-        ignores: ["index.page"],
-      },
-    ],
-  },
 };
